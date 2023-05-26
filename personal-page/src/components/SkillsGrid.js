be37737dda811935f8fa7853611e1a82c02156a0
royap <royaparsa294@gmail.com>
2023-05-26 09:33:30 -0400

    added growth when hovering over icons

--- personal-page/src/components/SkillsGrid.js
+++ personal-page/src/components/SkillsGrid.js
@@ -6,7 +6,7 @@ const SkillsGrid = () => {
       <div className="grid grid-cols-4 gap-4">
         {skills.map((skill, index) => (
           <div key={index} className="flex flex-col items-center">
-            {React.createElement(skill.icon, { style: { color: skill.color }, className: 'w-12 h-12 mb-2' })}
+            {React.createElement(skill.icon, { style: { color: skill.color }, className: 'w-12 h-12 mb-2 transform h-64  transition duration-500 hover:scale-125 hover:0 flex justify-center items-center' })}
             <span>{skill.title}</span>
           </div>
         ))}
